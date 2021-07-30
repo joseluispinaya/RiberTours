74f4e4c3df7898d7ecfb0b222bf2f3686817fdbc
joseluispinaya <joseluisdelta1@gmail.com>
2021-07-30 00:30:52 -0400

    Add navigation

---
+++ screens/TopTours.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function TopTours() {
+    return (
+        <View>
+            <Text>TopTours...</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
---
+++ screens/tours/Tours.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function Tours() {
+    return (
+        <View>
+            <Text>Tours...</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
